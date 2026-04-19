dbcde0597efaf0e3848df7bc2c4bbad59b071fa2
coroaiphotobooth <coroaiphotobooth@gmail.com>
2026-04-19 15:56:31 +0700

    feat: Update VIP apps script URL
    The Google Apps Script URL for VIP features has been updated. This change ensures that the application uses the correct endpoint for VIP functionality.

--- constants.tsx
+++ constants.tsx
@@ -39,7 +39,7 @@ export const DEFAULT_SETTINGS: PhotoboothSettings = {
   printMethod: 'direct', // Default direct
   enableDoublePrint: false, // Default off
   enableVipMode: false,
-  vipAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbyA0dQlOTj9qBi8EW3u97aKCJb-069fldqjl4rCPzTW-01uxOW7NSELPJBwAvAsikK_/exec',
+  vipAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbw5ZUzv-XwzgYJPvQt_PN42Yof3NivR_V3TJ3mfa6XkhsmAiOHMzZ5OTjA2NrKQk8s8/exec',
   vipVideoIdleUrl: 'https://ufxymelzgxshoopuphoj.supabase.co/storage/v1/object/public/DATA%20COROAI/VIDEO/LOOP%20IDLE.mp4',
   vipVideoTalkingUrl: 'https://ufxymelzgxshoopuphoj.supabase.co/storage/v1/object/public/DATA%20COROAI/VIDEO/TALKING.mp4',
 };
